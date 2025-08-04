6d70470a94ede343ef85b3bc0a48e13a8c41b41a
Nahim2003 <nahimahmed1000@outlook.com>
2025-08-04 14:49:21 +0100

    Trigger workflow by updating main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+region= "us-west-2"
+  version = "~> 4.0"
+}
+
+
+resource "aws_s3_bucket" "example" {
+ bucket = "my-terraform-bucket-12345"
+
+acl = "private"
+
+tags = {
+Environment= "Dev"
+Name= "My bucket"
+}
+}
+# dummy change
