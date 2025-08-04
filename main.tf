987bd976365a7713d53146a7cbd238752dcf6f90
Nahim2003 <nahimahmed1000@outlook.com>
2025-08-04 14:59:50 +0100

    Fix: applied best practices and version constraints

--- main.tf
+++ main.tf
@@ -1,17 +1,25 @@
-provider "aws" {
-  region  = "us-west-2"
-  version = "~> 4.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
 }
 
+provider "aws" {
+  region = "us-west-2"
+}
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-terraform-bucket-12345"
-
-  acl = "private"
+  acl    = "private"
 
   tags = {
     Environment = "Dev"
     Name        = "My bucket"
   }
 }
-# dummy change
+
