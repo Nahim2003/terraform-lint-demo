f0bc37c55296d66b526e81b181143204e0e530f6
Nahim2003 <nahimahmed1000@outlook.com>
2025-08-04 14:52:59 +0100

    Fix: format Terraform code

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-region= "us-west-2"
+  region  = "us-west-2"
   version = "~> 4.0"
 }
 
 
 resource "aws_s3_bucket" "example" {
- bucket = "my-terraform-bucket-12345"
+  bucket = "my-terraform-bucket-12345"
 
-acl = "private"
+  acl = "private"
 
-tags = {
-Environment= "Dev"
-Name= "My bucket"
-}
+  tags = {
+    Environment = "Dev"
+    Name        = "My bucket"
+  }
 }
 # dummy change
